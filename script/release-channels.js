03b3b8a96130dc6541e0c8b392adb01c7e20aca8
pbochynski <p.bochynski@gmail.com>
2023-11-08 11:18:22 +0100

    Initial commit

---
+++ script/release-channels.js
@@ -0,0 +1,32 @@
+const jsyaml = require('js-yaml')
+const fs = require('fs')
+const channels = require('../channels.json')
+
+async function loadModules(modules) {
+  for (let m of modules) {
+    let url = m.deploymentYaml
+    let response = await fetch(url)
+    let body = await response.text()
+    m.resources = []
+    jsyaml.loadAll(body, (doc) => {
+      m.resources.push({ resource: doc })
+    });
+
+    url = m.crYaml
+    response = await fetch(url)
+    body = await response.text()
+    m.cr = { resource: jsyaml.load(body) }
+    m.cr.resource.metadata.namespace='kyma-system'
+  }
+}
+
+async function releaseChannels(){
+  for(let ch of channels) {
+    await loadModules(ch.modules)
+    console.log("channel loaded")
+    fs.writeFileSync(`${ch.name}.json`,JSON.stringify(ch.modules,null,2))
+    console.log("channel written")
+  }
+}
+
+releaseChannels();
